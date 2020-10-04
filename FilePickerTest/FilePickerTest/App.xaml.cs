159b999afdf8599cfa77b3b557dcec478dbcdec1
gwise <pulmuone.ji@gmail.com>
2020-10-04 09:42:32 +0900

    Add project files.

---
+++ FilePickerTest/FilePickerTest/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using FilePickerTest.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FilePickerTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new MediaPickerPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FilePickerTest/FilePickerTest/ViewModels/BaseViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilePickerTest.ViewModels
+{
+    public class BaseViewModel : ObservableObject
+    {
+        bool isBusy;
+
+        public bool IsBusy
+        {
+            get => isBusy;
+            set => SetProperty(ref isBusy, value, onChanged: () => OnPropertyChanged(nameof(IsNotBusy)));
+        }
+
+        public bool IsNotBusy => !IsBusy;
+
+        public virtual void OnAppearing()
+        {
+        }
+
+        public virtual void OnDisappearing()
+        {
+        }
+
+        internal event Func<string, Task> DoDisplayAlert;
+
+        public Task DisplayAlertAsync(string message)
+        {
+            return DoDisplayAlert?.Invoke(message) ?? Task.CompletedTask;
+        }
+    }
+}
